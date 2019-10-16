4a52ca0a980e440cf73ade86a2d8746aedc79071
Nahvin <55419300+Nahvin00@users.noreply.github.com>
2019-10-16 17:32:11 +0800

    Update One.cpp

--- One.cpp
+++ One.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main() {
-int flo,i,room,oroom;
-double perc,t_room=0,t_oroom=0;
-cout<<"Enter the number of floor in the hotel: "<<endl;
+int flo,i,room,oroom; //declaration of variables
+double perc,t_room=0,t_oroom=0; //certain variables are declared as double because decimal point is needed in the final result
+cout<<"Enter the number of floor in the hotel: "<<endl; //prompt user to input
 cin>>flo;
-for(i=1;i<=flo;i++){
+for(i=1;i<=flo;i++){ //for loop to enter the number of rooms in each floor as well as to sum it up
     cout<<"Enter number of rooms in floor "<<i<<":"<<endl;
     cin>>room;
     cout<<"Enter number of occupied rooms in floor "<<i<<":"<<endl;
     cin>>oroom;
-    t_room+=room;
-    t_oroom+=oroom;}
-perc=(t_oroom)/(t_room)*100;     
-cout<<"The total number of rooms in the hotel is "<<t_room<<endl;
+    t_room+=room; //increament of the total number of room
+    t_oroom+=oroom;} //increament of the total number of occupied room
+perc=(t_oroom)/(t_room)*100;  // formulae to calculate the percentage of occupied room   
+cout<<"The total number of rooms in the hotel is "<<t_room<<endl; //output of the results
 cout<<"The total number of occupied rooms in the hotel is "<<t_oroom<<endl;
 cout<<"The percentage of occupied room is "<<perc<<"%"<<endl;
 cout<<"End of Program.";
 
-    return 0;
+    return 0; //return value of zero
 }
